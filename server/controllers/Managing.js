4f4934a88cd26ae9fc3df23bde0d1b1e1dc7d54d
Shivam Aggarwal <shivamaggarwal1821@gmail.com>
2024-07-21 03:27:01 +0530

    Removed bug

--- server/controllers/Managing.js
+++ server/controllers/Managing.js
@@ -9,7 +9,6 @@ export const AddLeetcodeId = async (req, res) => {
     try {
         const userId = req.user.id;
         const { leetcodeId } = req.body;
-        console.log("basic data",leetcodeId,userId);
 
         if (!userId || !leetcodeId) {
             return res.status(400).json({
@@ -17,9 +16,6 @@ export const AddLeetcodeId = async (req, res) => {
                 message: "Insufficient data",
             });
         }
-        
-        
-        
         const user = await User.findById(userId);
         if (!user) {
             return res.status(404).json({
@@ -27,7 +23,6 @@ export const AddLeetcodeId = async (req, res) => {
                 message: "User does not exist",
             });
         }
-        console.log("user found")
 
         let leetcodeDetails = await LeetcodeID.findOne({
             username: leetcodeId,
@@ -35,14 +30,13 @@ export const AddLeetcodeId = async (req, res) => {
         if (!leetcodeDetails) {
             leetcodeDetails = await LeetcodeID.create({
                 username: leetcodeId,
-            }).populate("stats");
+            });
         }
 
         const leetcodeIdPresent = user.linkedto.some(
             (lId) => lId && lId.equals(leetcodeDetails._id)
         );
 
-        console.log("reached");
         if (leetcodeIdPresent) {
             return res.status(400).json({
                 success: false,
@@ -57,13 +51,11 @@ export const AddLeetcodeId = async (req, res) => {
                 message: "Invalid Leetcode ID",
             });
         }
-        console.log(response);
 
         const currDate = dateFinder();
         if (user.linkedto.length === 0) {
             user.startDate = currDate;
         }
-        console.log("ehre");
 
         const organizedStats = preprocessStats(
             response,
