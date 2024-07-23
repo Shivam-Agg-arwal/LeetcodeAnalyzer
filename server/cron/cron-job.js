757c6145df5bebd0a45c2f29893e47f9ae376094
Shivam Aggarwal <shivamaggarwal1821@gmail.com>
2024-07-23 10:05:53 +0530

    changed for testing

--- server/cron/cron-job.js
+++ server/cron/cron-job.js
@@ -2,7 +2,7 @@
 import cron from 'node-cron';
 import { autoUpdate } from '../controllers/updateController'; // Importing the function
 
-cron.schedule('0 22 * * *', async () => {
+cron.schedule('* * * * *', async () => {
     try {
         await autoUpdate(); // Call the function
         console.log('Auto update ran successfully at 10 PM');
